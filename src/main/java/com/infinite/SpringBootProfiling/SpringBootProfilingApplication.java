6ca418fcc805e3f28537a2d86dc3c8035f38c8a1
vasanthi <vasanthi>
2023-11-20 10:39:23 +0530

    c1

---
+++ src/main/java/com/infinite/SpringBootProfiling/SpringBootProfilingApplication.java
@@ -0,0 +1,12 @@
+package com.infinite.SpringBootProfiling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfilingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProfilingApplication.class, args);
+	}
+}
